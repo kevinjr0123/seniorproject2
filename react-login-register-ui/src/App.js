03f0d2770524305765039e6ab4a6a5bb3431571f
Kevin <kevinclaiborne83@.com>
2023-02-11 19:37:17 -0500

    added home page ui

--- react-login-register-ui/src/App.js
+++ react-login-register-ui/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 
 import Login from './components/login.component';
 import SignUp from './components/signup.component';
+import HomePage from './components/homepage.component';
 
 
 function App() {
@@ -14,18 +15,23 @@ function App() {
         <nav className= "navbar navbar-expand-lg navbar-light fixed-top">
           <div className="container">
             <Link className="navbar-brand" to={'/sign-in'}>
-              Cigna
+              Remedy
             </Link>
             <div className="collaspe navbar-collapse" id="navbarTogglerDemo02">
               <ul className="navbar-nav ml-auto">
                 <li className="nav-item">
                   <Link className="nav-link" to={'/sign-in'}>
-                    Sign in
+                    Log in
                   </Link>
                 </li>
                 <li className="nav-item">
                   <Link className="nav-link" to={'/sign-up'}>
-                    Sign up
+                    Register
+                  </Link>
+                </li>
+                <li className="nav-item">
+                  <Link className="nav-link" to={'/home-page'}>
+                  Home Page 
                   </Link>
                 </li>
               </ul>
@@ -39,6 +45,7 @@ function App() {
             <Route exact path='/' element={<Login />} />
             <Route path='/sign-in' element={<Login />} />
             <Route path='/sign-up' element={<SignUp />} />
+            <Route path='/home-page' element={<HomePage />} />
           </Routes>
         </div>
       </div>
